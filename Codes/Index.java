ad149687c62555d64ef07d390fbe0a1bf51dca5c
iffishells <iffishells@gmail.com>
2020-12-03 22:41:30 +0500

    Added Code's Folder'

---
+++ Codes/Index.java
@@ -0,0 +1,53 @@
+package Codes;
+
+
+class TOC{
+    
+    TOC()
+    {
+        System.out.println("Transfer of order Called ...");
+    }
+}
+class Onwers{
+    Onwers()
+    {
+        System.out.println("Onwers  Called ...");
+    }
+}
+class FilerNonFiler{
+
+    FilerNonFiler()
+    {
+        System.out.println("filerNonFiler has been called ...");
+    }
+}
+
+class Vehicle {
+
+    Vehicle(){
+        System.out.println("Vehicle has been callled ,,,");
+
+    }
+}
+class Numbers{
+    
+    Numbers(){
+        Sytstem.out.println("Numbers has called");
+
+    }
+}
+
+class Fees {
+    
+    Fees(){
+        System.out.println("Fees Called Now");
+    }
+}
+
+public class Index {
+    int x = 5;
+  
+    public static void main(String[] args) {
+      System.out.println("Working....");
+    }
+  }
