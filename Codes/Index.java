e3eaca62454d5a7c3d92115722e3330509696bdc
iffishells <iffishells@gmail.com>
2020-12-03 23:15:13 +0500

    Updated index class

--- Codes/Index.java
+++ Codes/Index.java
@@ -1,53 +1,17 @@
 package Codes;
-
-
-class TOC{
-    
-    TOC()
-    {
-        System.out.println("Transfer of order Called ...");
-    }
-}
-class Onwers{
-    Onwers()
-    {
-        System.out.println("Onwers  Called ...");
-    }
-}
-class FilerNonFiler{
-
-    FilerNonFiler()
-    {
-        System.out.println("filerNonFiler has been called ...");
-    }
-}
-
-class Vehicle {
-
-    Vehicle(){
-        System.out.println("Vehicle has been callled ,,,");
-
-    }
-}
-class Numbers{
-    
-    Numbers(){
-        Sytstem.out.println("Numbers has called");
-
-    }
-}
-
-class Fees {
-    
-    Fees(){
-        System.out.println("Fees Called Now");
-    }
-}
-
+import java.util.Scanner;
+import Codes.TOC;
+/**
+ * Index
+ */
 public class Index {
-    int x = 5;
-  
+
     public static void main(String[] args) {
-      System.out.println("Working....");
+        System.out.println("Start in the Name of Allah.......");
+
+        TOC TofObject = new TOC();
+        TofObject.SetVehicleEngineNumber();
+
     }
-  }
+    
+}
