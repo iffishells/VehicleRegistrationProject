8fa5ac4bb6f572a27097e533bbe630924a6c99d0
iffishells <iffishells@gmail.com>
2020-12-21 22:40:17 +0500

    Code is completed

---
+++ Main-Code/src/gui/Transfer_of_order.java
@@ -0,0 +1,95 @@
+package gui;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import javax.xml.crypto.dsig.keyinfo.RetrievalMethod;
+
+import com.itextpdf.text.pdf.PdfStructTreeController.returnType;
+
+public class Transfer_of_order {
+
+    private String Older_owner_name;
+    private String New_owner_name;
+    private String Older_Owner_CNIC;
+    private String New_Owner_CNIC;
+    private String Vehicle_Number;
+
+    public void Set_older_owner_name(String word) {
+
+        Older_owner_name = word;
+    }
+
+    public void Set_new_owner_name(String word) {
+
+        New_owner_name = word;
+    }
+
+    public void Set__new_CNIC_owner(String word) {
+
+        New_Owner_CNIC = word;
+    }
+
+    public void Set_Older_CNIC_owner(String word) {
+        Older_Owner_CNIC = word;
+    }
+
+    public void Set_vehcile_number(String word) {
+        Vehicle_Number = word;
+    }
+
+
+
+    public String Get_older_owner_name() {
+
+        return Older_owner_name;
+
+    }
+
+    public String Get_new_owner_name() {
+        
+
+        return New_owner_name;
+    }
+
+    public String Get__new_CNIC_owner() {
+
+        return New_Owner_CNIC;
+    }
+
+    public String Get_Older_CNIC_owner() {
+        return Older_Owner_CNIC;
+    }
+
+    public String Get_vehcile_number() {
+        return Vehicle_Number;
+    }
+
+
+    public boolean check(Transfer_of_order ob) throws FileNotFoundException {
+        ///check if its registered or not/
+
+        Scanner read = new Scanner(new FileInputStream("/home/iffishells/eclipse-workspace/GUI/Database.txt"));
+        String words =  ob.Get_vehcile_number();                          //ob.Get_vehcile_number();
+
+            while(read.hasNextLine())
+            {
+                String line = read.nextLine();
+                
+                    if(line.indexOf(words)!=-1) {
+                        return true;   //1 is True
+                        
+                    }
+                
+           }    
+    
+        return false;  // o is false
+        
+    }
+
+    
+    
+
+
+}
